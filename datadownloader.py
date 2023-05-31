a9a9b42d188c1a16e35ac054e71bc671bb9d811a
Polatkan Polat <poliska5@hotmail.com>
2023-05-31 13:38:05 +0300

    add data downloader script

---
+++ datadownloader.py
@@ -0,0 +1,46 @@
+from icrawler.builtin import GoogleImageCrawler
+import argparse
+import os
+
+
+class ImageDownloader:
+    """
+    This class uses icrawler library and lets you to download
+    images from google
+    """
+
+    def __init__(self, output_directory="data") -> None:
+        self.output_directory = output_directory
+        self.createDirectoryIfNotExists(self.output_directory)
+
+    def createDirectoryIfNotExists(self, dirname):
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+    def downloadByName(self, name, number=100):
+        download_path = os.path.join(self.output_directory, name)
+        if os.path.exists(download_path):
+            print(name, "Directory already exists")
+            if len(os.listdir(download_path)) >= 100:
+                print(name, "Directory already contains at least 100 images")
+                return
+        else:
+            self.createDirectoryIfNotExists(download_path)
+        google_crawler = GoogleImageCrawler(
+            feeder_threads=1,
+            parser_threads=2,
+            downloader_threads=4,
+            storage={"root_dir": download_path},
+        )
+        google_crawler.crawl(keyword=name, max_num=number, file_idx_offset=0)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="downloads some images from google")
+    parser.add_argument(
+        "--number", type=int, help="number of images to download", default=20
+    )
+    parser.add_argument("name", help="description of images to download")
+    args = parser.parse_args()
+    downloader = ImageDownloader()
+    downloader.downloadByName(args.name, args.number)
