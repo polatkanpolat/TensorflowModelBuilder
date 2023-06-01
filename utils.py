de671ffb33334286cda30f513edea2e984540132
Polatkan Polat <poliska5@hotmail.com>
2023-06-01 15:56:16 +0300

    add returnClassPaths function to utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,15 @@
 import os
+from glob import glob
 
 
 def createDirectoryIfNotExists(dirname):
     if not os.path.exists(dirname):
         os.makedirs(dirname)
+
+
+def returnClassPaths(className, rootDir="data"):
+    classPathJPG = os.path.join(rootDir, className, "*.jpg")
+    classPathJPEG = os.path.join(rootDir, className, "*.jpeg")
+    classPathPNG = os.path.join(rootDir, className, "*.png")
+    classPathBMP = os.path.join(rootDir, className, "*.bmp")
+    return glob(classPathJPG) + glob(classPathJPEG) + glob(classPathPNG) + glob(classPathBMP)
